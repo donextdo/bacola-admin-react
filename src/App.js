f3cd172df9406f10818e4639e26a610ea658eecb
charuka97 <charukatharindhu@gmail.com>
2023-10-04 13:46:51 +0530

    Bacola admin panel initial project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Bacola Admin Panel
+        </h2>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
