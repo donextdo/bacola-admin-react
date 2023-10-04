6c3a2af37ed0247f633c176986ab988df8ec93ef
charuka97 <charukatharindhu@gmail.com>
2023-10-04 15:01:31 +0530

    complete default side navbar with mui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-
 import './App.css';
+import ResponsiveDrawer from './Components/sideNavBar'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>
-          Bacola Admin Panel
-        </h2>
-      
-      </header>
+      <ResponsiveDrawer />
     </div>
   );
 }
